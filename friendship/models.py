d16dd085e85c045a886a2699f8fbc5c024677279
mohamma8d1 <mmmirnoori@gmail.com>
2024-10-08 02:25:06 +0330

    Reformat Models

--- friendship/models.py
+++ friendship/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 from django.conf import settings
+
+
 class Friendship(models.Model):
-    request_from = models.ForeignKey(to=settings.AUTH_USER_MODEL , on_delete=models.PROTECT , related_name='friend_request_from')
-    request_to = models.ForeignKey(to=settings.AUTH_USER_MODEL , on_delete=models.PROTECT, related_name='friend_request_to')
+    request_from = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.PROTECT,
+                                     related_name='friend_request_from')
+    request_to = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.PROTECT,
+                                   related_name='friend_request_to')
     accepted = models.BooleanField(default=False)
     created_time = models.DateTimeField(auto_now_add=True)
     updated_time = models.DateTimeField(auto_now=True)
@@ -10,4 +14,4 @@ class Friendship(models.Model):
     class Meta:
         verbose_name = 'Friendship'
         verbose_name_plural = 'Friendship'
-        unique_together = ('request_from' , 'request_to')
+        unique_together = ('request_from', 'request_to')
